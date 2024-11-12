32fbb080e8668a1cc419575960cc24060fc0d4f2
sidmohanty11 <sidmohanty11@gmail.com>
2024-11-12 15:11:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component,
+  Input,
+  ViewContainerRef,
+  ViewChild,
+  TemplateRef,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Content, fetchOneEntry } from '@builder.io/sdk-angular';
+
+// ====================================== this is the issue ================================================
+@Component({
+  selector: 'complete-component',
+  standalone: true,
+  template: `{{ label }}: <ng-content></ng-content>`,
+})
+export class CompleteComponent {
+  @Input() label!: string;
+}
+
+@Component({
+  selector: 'example',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <ng-template #ahoj> hello<ng-content></ng-content></ng-template>
+    <ng-container
+      *ngComponentOutlet="
+        CompleteComponent;
+        inputs: myInputs;
+        content: myContent
+      "
+    ></ng-container>`,
+})
+export class NgComponentOutletCompleteExample {
+  CompleteComponent = CompleteComponent;
+
+  myInputs = { label: 'Complete' };
+
+  @ViewChild('ahoj', { static: true }) ahojTemplateRef!: TemplateRef<any>;
+  myContent?: any[][];
+
+  constructor(private vcr: ViewContainerRef) {}
+
+  ngOnInit() {
+    this.myContent = [
+      this.vcr.createEmbeddedView(this.ahojTemplateRef).rootNodes,
+    ];
+  }
+}
+
+// =========================================================================================================
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // issue --------------------------------------------------------------------------------------------------------
+  imports: [CommonModule, NgComponentOutletCompleteExample, CompleteComponent],
+  template: ` <example /> `,
+  // --------------------------------------------------------------------------------------------------------
+  // template: `<builder-content
+  //   model="page"
+  //   apiKey="ad30f9a246614faaa6a03374f83554c9"
+  //   [content]="content"
+  // />`,
+  // imports: [Content],
 })
 export class AppComponent {
-  title = 'ssr-app';
+  content = null as any;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,9 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    // if we comment this out, the app works on the server
+    provideClientHydration(),
+  ],
 };
